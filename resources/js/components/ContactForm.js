0a76b81c8739ab09b234e2810dc877b11b420a21
Stefan <stefandjokic@pm.me>
2019-10-01 17:43:23 +0200

    Fixed path to Helpers.js

--- resources/js/components/ContactForm.js
+++ resources/js/components/ContactForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { areInputsValid } from '../../Helpers';
+import { areInputsValid } from '../Helpers';
 import axios from 'axios';
 import { AlertContext } from '../../app';
 
